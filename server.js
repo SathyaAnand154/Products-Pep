1be439ebb17586fa03afa4b7feb8987b251ee6d6
sathya anand <sathyaanand2005@gmail.com>
2026-02-05 16:19:48 +0530

    Remove proxy restrictions and add CORS headers for full access

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from "express";
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 /* 👇 THIS IS THE KEY */
 app.use(express.static("public"));
